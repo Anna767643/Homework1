b41cbe4e81718dacd94117f7dbfd4d4a85df23b9
Anna <rozetka_1996@mail.ru>
2023-11-09 10:06:26 +0700

    MinMax

---
+++ MinMax/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+//Ввод двух чисел
+Console.Write("Enter the first number: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int number2 = int.Parse(Console.ReadLine());
+
+//Определение меньшего и большего числа, вывод 
+
+if (number1 > number2)
+{
+    Console.WriteLine($"max = {number1}, min = {number2}");
+}
+else 
+{
+    Console.WriteLine($"max = {number2}, min = {number1}");
+}
