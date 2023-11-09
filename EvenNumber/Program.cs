dc67f1fbf98e7f59f1e2029585661dca4b71e15f
Anna <rozetka_1996@mail.ru>
2023-11-09 10:40:07 +0700

    Even number

---
+++ EvenNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+//Ввод числа
+Console.Write("Enter the number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0) 
+{
+    Console.WriteLine($"Tne number {number} is even");
+}
+else
+{
+     Console.WriteLine($"Tne number {number} is NOT even");  
+}
