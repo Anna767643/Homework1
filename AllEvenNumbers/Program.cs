a2db914b9b763c4e8f3b1f1fe409260c4152699e
Anna <rozetka_1996@mail.ru>
2023-11-09 10:47:13 +0700

    All even numbers

---
+++ AllEvenNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//Ввод числа
+Console.Write("Enter the number: ");
+int number = int.Parse(Console.ReadLine());
+
+//Цикл
+int evenNumber = 2;
+while (evenNumber <= number)
+{
+    Console.Write($"{evenNumber}, ");
+    evenNumber = evenNumber + 2;
+}
