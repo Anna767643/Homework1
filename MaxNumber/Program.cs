39f879b1c8de42329f59f49e3a1a1eaa72a360c7
Anna <rozetka_1996@mail.ru>
2023-11-09 10:25:33 +0700

    Max number

---
+++ MaxNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+//Ввод чисел
+Console.Write("Enter the first number: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Enter the third number: ");
+int number3 = int.Parse(Console.ReadLine());
+
+//Определение максимального числа и вывод
+int max = number1;
+if (number2 > max)  max = number2;
+if (number3 > max)  max = number3;
+
+Console.WriteLine($"max = {max}");
+
